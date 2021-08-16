36c7df04093e51f799464191e70d147b72098b1f
YEVHEN HOLOVASHEV <golovashev.ks@gmail.com>
2021-08-16 12:16:54 +0300

    Create azurestacksnapshot.ps1

---
+++ azurestacksnapshot.ps1
@@ -0,0 +1,43 @@
+# Connect to Azure StackHub
+
+Add-AzEnvironment -Name "yevgen.golovashev@kyivstarsandbox.onmicrosoft.com" -ArmEndpoint "https://management.kyiv.azure.kyivstar.ua"
+Connect-AzAccount -EnvironmentName "yevgen.golovashev@kyivstarsandbox.onmicrosoft.com"
+
+#   Tags varibles 
+
+$tag = @{
+    "owner" = "golovasheveo";
+    "project" = "backup";
+}
+
+$resourceGroupName = 'rg-snaptest' ;
+$location = 'kyiv' ;
+
+$disks = Get-AzDisk -ResourceGroupName $resourceGroupName;
+
+
+foreach ($var in $disks) {
+    $Date = Get-Date -Format "yy/MM/ddTHH:mm:ss" | ForEach-Object { $_ -replace "[^0-9a-zA-Z]+", "" } ;
+    $snapshotName = 'Snapshot-'+$var.Name+'-'+$Date;
+
+    try {
+#   Creates a configurable snapshot object
+    $snapshot = New-AzSnapshotConfig `
+    -SourceUri $var.id `
+    -Location $location `
+    -CreateOption copy `
+    -Tag $tag
+
+#   Creates a snapshot
+    New-AzSnapshot `
+    -Snapshot $snapshot `
+    -SnapshotName $snapshotName `
+    -ResourceGroupName $resourceGroupName `
+    }
+    catch {
+        Write-Error "An error occurred :";
+        Write-Error $snapshotName;
+        Write-Error $_;
+    }
+
+}
